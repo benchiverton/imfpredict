2e0c2230c60663a1acd6073f972a696afc8c23f7
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2022-02-13 14:41:40 +0000

    Add normalizer + tests

---
+++ Scripts/pyplot_example.py
@@ -0,0 +1,29 @@
+import datetime
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+import imfprefict.config as config
+
+
+def get_data(config):
+    base = datetime.datetime.today().date()
+    data_date = [base - datetime.timedelta(days=x) for x in range(1000)]
+    data_close_price = np.random.rand(1000)
+    num_data_points = 1000
+    display_date_range = data_date[0].strftime("%d/%m/%Y") + " - " + data_date[9].strftime("%d/%m/%Y")
+
+    return data_date, data_close_price, num_data_points, display_date_range
+
+
+if __name__ == "__main__":
+    data_date, data_close_price, num_data_points, display_date_range = get_data(config)
+
+    fig = figure(figsize=(25, 5), dpi=80)
+    fig.patch.set_facecolor((1.0, 1.0, 1.0))
+    plt.plot(data_date, data_close_price, color=config.plotSettings["color_actual"])
+    plt.title("Plot test for data" + ", " + display_date_range)
+    plt.grid(visible=None, which='major', axis='y', linestyle='--')
+    plt.show()
---
+++ imfprefict/config.py
@@ -0,0 +1,24 @@
+plotSettings = {
+    "xticks_interval": 90,  # show a date every 90 days
+    "color_actual": "#001f3f",
+    "color_train": "#3D9970",
+    "color_val": "#0074D9",
+    "color_pred_train": "#3D9970",
+    "color_pred_val": "#0074D9",
+    "color_pred_test": "#FF4136",
+}
+
+modelSettings = {
+    "input_size": 1,  # since we are only using 1 feature, close price
+    "num_lstm_layers": 2,
+    "lstm_size": 32,
+    "dropout": 0.2,
+},
+
+trainingSettings = {
+    "device": "cpu",  # "cuda" or "cpu"
+    "batch_size": 64,
+    "num_epoch": 100,
+    "learning_rate": 0.01,
+    "scheduler_step_size": 40,
+}
