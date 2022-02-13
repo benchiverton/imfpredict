05b81437c27f76e6439d6bc79e7175fa6ce00635
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2022-02-13 15:16:06 +0000

    Add more unit test cases for normalizer

--- Tests/test_normalizer.py
+++ Tests/test_normalizer.py
@@ -6,7 +6,13 @@ from imfprefict import Normalizer
 
 class TestRead:
     @pytest.mark.parametrize("fit_transform_data", [
-        {"x": [10, 20, 30], "normalized_x": [-1.224744871391589, 0, 1.224744871391589]}
+        {"x": [-1, 1], "normalized_x": [-1, 1]},  # only possible way to have mean 0 SD 1 for 2 data points is -1, 1
+        {"x": [1, -1], "normalized_x": [1, -1]},  # only possible way to have mean 0 SD 1 for 2 data points is -1, 1
+        {"x": [56565, 945864596745], "normalized_x": [-1, 1]},  # only possible way to have mean 0 SD 1 for 2 data points is -1, 1
+        {"x": [-2, -1, 15], "normalized_x": [-0.7703288865196433, -0.6419407387663694, 1.4122696252860127]},
+        {"x": [10, 20, 30], "normalized_x": [-1.224744871391589, 0, 1.224744871391589]},
+        {"x": [-2, -2, 2, 2], "normalized_x": [-1, -1, 1, 1]},
+        {"x": [-5, -5, -5, 5, 5, 5], "normalized_x": [-1, -1, -1, 1, 1, 1]},
     ])
     def test_fit_transform(self, fit_transform_data):
         scaler = Normalizer()
