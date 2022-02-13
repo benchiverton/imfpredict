83dc9d9a83e96f6d4e11056992fb33bf4324ca8f
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2022-02-13 19:18:00 +0000

    Add tests for window method

---
+++ Tests/test_dataPreperation.py
@@ -0,0 +1,18 @@
+"""Tests for imfprefict.dataPreperation module."""
+
+import pytest
+
+import numpy as np
+
+import imfprefict.dataPreperation as dp
+
+
+class TestRead:
+    @pytest.mark.parametrize("window_data", [
+        {"x": np.array([1, 2, 3, 4, 5]), "windowSize": 1, "windowed_x": [1, 2, 3, 4, 5]},
+        {"x": np.array([1, 2, 3, 4, 5]), "windowSize": 2, "windowed_x": [[1, 2], [2, 3], [3, 4], [4, 5]]},
+    ])
+    def test_window_data(self, window_data):
+        data_x, data_x_unseen = dp.window_data(window_data["x"], window_data["windowSize"])
+        for datapoint, windowed_datapoint in zip(data_x, window_data["windowed_x"]):
+            np.testing.assert_allclose(datapoint, windowed_datapoint, rtol=1e-5, atol=0)
