ff3e3d27f32ef2319d2037036128fbef589ffca7
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2022-02-15 20:32:04 +0000

    Add additional tests for windowing

--- Tests/test_dataPreperation.py
+++ Tests/test_dataPreperation.py
@@ -11,6 +11,9 @@ class TestRead:
     @pytest.mark.parametrize("window_data", [
         {"x": np.array([1, 2, 3, 4, 5]), "windowSize": 1, "windowed_x": [1, 2, 3, 4, 5]},
         {"x": np.array([1, 2, 3, 4, 5]), "windowSize": 2, "windowed_x": [[1, 2], [2, 3], [3, 4], [4, 5]]},
+        {"x": np.array([1, 2, 3, 4, 5]), "windowSize": 3, "windowed_x": [[1, 2, 3], [2, 3, 4], [3, 4, 5]]},
+        {"x": np.array([1, 2, 3, 4, 5]), "windowSize": 4, "windowed_x": [[1, 2, 3, 4], [2, 3, 4, 5]]},
+        {"x": np.array([1, 2, 3, 4, 5]), "windowSize": 5, "windowed_x": [[1, 2, 3, 4, 5]]},
     ])
     def test_window_data(self, window_data):
         data_x, data_x_unseen = dp.window_data(window_data["x"], window_data["windowSize"])
